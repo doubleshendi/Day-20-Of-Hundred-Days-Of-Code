59cce89018819b96161a92a19c33a57498b8a8dc
doubleshendi <78292834+doubleshendi@users.noreply.github.com>
2024-05-21 01:15:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#for i in range(100,110):
+   #print(i)
+#print("Thirteen Times Table")
+#for i in range(1, 13) :
+  #print(i, "x 13 =" , i * 13)
+
+#for i in range(0, 10000000000000000000000, 25):
+   #print(i)
+
+#for i in range(10, 0, -1):
+   #print(i)
+
+print("List generator")
+print()
+start = int(input("Start at: "))
+end = int(input("End before: "))
+increment = int(input("Increment between: "))
+for i in range(start, end, increment):
+  print(i) 
+  
+                
+
